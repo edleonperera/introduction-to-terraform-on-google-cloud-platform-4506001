45a0cae3db05b7f70465344202a8d3a9811a41fc
edleonperera <163498198+edleonperera@users.noreply.github.com>
2024-03-15 18:37:00 +0800

    Website

--- main.tf
+++ main.tf
@@ -5,29 +5,28 @@ module "app_network" {
   network_name = "${var.network_name}-network"
   project_id   = var.project_id
   subnets = [
-        {
-            subnet_name = "${var.network_name}-subnet0"
-            subnet_ip = var.network_ip_range
-            subnet_region = var.region
-        }
-        ]
-}
-
-
-ingress_rules = [
     {
-    name  = "${var.network_name}-web'
-    description = "Inbound web"
-    source_ranges = ["0.0.0.0/0"]
-    target_tags = ["${var.network_name}-web"]
-    allow = [
-      {
-      protocol = "tcp"
-      ports    = ["80","443"]
-      }
-    ]
-  }
- ]
+      subnet_name   = "${var.network_name}-subnet0"
+      subnet_ip     = var.network_ip_range
+      subnet_region = var.region
+    }
+  ]
+
+  ingress_rules = [
+    {
+      name          = "${var.network_name}-web"
+      description   = "Inbound web"
+      source_ranges = ["0.0.0.0/0"]
+      target_tags   = ["${var.network_name}-web"]
+      allow = [
+        {
+          protocol = "tcp"
+          ports    = ["80","443"]
+        }
+      ]
+    }
+  ]
+}
 
 data "google_compute_image" "ubuntu" {
   most_recent = true
@@ -38,19 +37,21 @@ data "google_compute_image" "ubuntu" {
 resource "google_compute_instance" "blog" {
   name         = var.app_name
   machine_type = var.machine_type
-  tags = ["${var.network_name}-web"]
+  tags         = ["${var.network_name}-web"]
+
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
     }
   }
+
   network_interface {
-   subnetwork = module.app_network.subnets_names[0]
-   access_config {
+    subnetwork = module.app_network.subnets_names[0]
+    access_config {
       # Leave empty for dynamic public IP
     }
   }
-  metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
-  allow_stopping_for_update = true
-}
 
+  metadata_startup_script    = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
+  allow_stopping_for_update  = true
+}
