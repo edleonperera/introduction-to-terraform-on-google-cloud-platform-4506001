009aee2e437bc91b759e6eb9976d9282592f5210
edleonperera <163498198+edleonperera@users.noreply.github.com>
2024-03-15 16:06:43 +0800

    Allow stopping

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ data "google_compute_image" "ubuntu" {
 resource "google_compute_instance" "web" {
   name         = "web"
   machine_type = "e2-micro"
-
   
   boot_disk {
     initialize_params {
@@ -30,5 +29,5 @@ resource "google_compute_instance" "web" {
       # Leave empty for dynamic public IP
     }
   }  
-
+  allow_stopping_for_update = true
 }
